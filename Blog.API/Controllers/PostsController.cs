bae71b6a25b0170d43e159140ec8a4829435328b
Fabiano Marcolin Maciel <fmaciel@glasslewis.com>
2024-10-10 18:33:20 +0100

    Post controller for API

---
+++ Blog.API/Controllers/PostsController.cs
@@ -0,0 +1,78 @@
+﻿using Blog.Core.Models;
+using Core.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostHandler _postHandler;
+        public PostsController(IPostHandler postHandler)
+        {
+            _postHandler = postHandler;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<PostModel>), StatusCodes.Status200OK)]
+        public async Task<IEnumerable<PostModel>> GetAsync()
+        {
+            return await _postHandler.GetAll();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(UserModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var user = await _postHandler.Get(id);
+            if (user == null || user.Id == 0)
+                return NotFound();
+            else
+                return Ok(user);
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        [ProducesResponseType(typeof(UserModel), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post([FromBody] PostModel model)
+        {
+            var newUser = await _postHandler.Add(model);
+            return CreatedAtAction("Get", new { id = newUser.Id }, newUser);
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(UserModel), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> PutAsync(int id, [FromBody] PostModel model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            await _postHandler.Edit(model);
+
+            return NoContent();
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(UserModel), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (!_postHandler.Exists(id))
+                return NotFound();
+
+            await _postHandler.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
--- Core/Handlers/PostHandler.cs
+++ Core/Handlers/PostHandler.cs
@@ -31,6 +31,8 @@ namespace Core.Handlers
             _context.Add(entity);
             await _context.SaveChangesAsync();
 
+            model.Id = entity.Id;
+
             return model;
         }
 
